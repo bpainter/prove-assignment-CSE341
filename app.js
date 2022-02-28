b42893eed8fe513474c6168b9d8c166323983371
Bermon Painter <git@bermonpainter.com>
2022-02-28 09:48:03 -0500

    Completed week05

--- app.js
+++ app.js
@@ -3,24 +3,49 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
+const flash = require('connect-flash');
 
 const config = require('./config');
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = `mongodb+srv://${config.mongodbAdmin}:${config.mongodbPassword}@cs341-bp.93yuc.mongodb.net/shop`;
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+});
+const csrfProtection = csrf();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({
+    secret: 'my secret',
+    resave: false,
+    saveUninitialized: false,
+    store: store
+  })
+);
+app.use(csrfProtection);
+app.use(flash());
 
 app.use((req, res, next) => {
-  User.findById('620b11180866cd2c4cb05cdd')
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
     .then(user => {
       req.user = user;
       next();
@@ -28,28 +53,21 @@ app.use((req, res, next) => {
     .catch(err => console.log(err));
 });
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrfToken();
+  next();
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
-  .connect(
-    `mongodb+srv://${config.mongodbAdmin}:${config.mongodbPassword}@cs341-bp.93yuc.mongodb.net/shop?retryWrites=true&w=majority`
-  )
+  .connect(MONGODB_URI)
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Bermon',
-          email: 'bermon@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
     // Possible fix for Heroku deploy issue with the Error R1 boot timeout
     const PORT = process.env.PORT | 3000;
     app.listen(PORT, () => {
