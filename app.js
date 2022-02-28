0f1b2b6a6321bed50ae04eea8de74a69da1f551b
Bermon Painter <git@bermonpainter.com>
2022-02-27 20:25:56 -0500

    04 Prove Complete

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
+const User = require('./models/user');
 
 const app = express();
 
@@ -16,13 +18,43 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) => {
+  User.findById('620b11180866cd2c4cb05cdd')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
+});
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-// Possible fix for Heroku deploy issue with the Error R1 boot timeout
-const PORT = process.env.PORT | 3000;
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
+mongoose
+  .connect(
+    'mongodb+srv://cse341-db-admin:HLBy2hdhXsaSoDNm@cs341-bp.93yuc.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Bermon',
+          email: 'bermon@test.com',
+          cart: {
+            items: []
+          }
+        });
+        user.save();
+      }
+    });
+    // Possible fix for Heroku deploy issue with the Error R1 boot timeout
+    const PORT = process.env.PORT | 3000;
+    app.listen(PORT, () => {
+        console.log(`Server listening on ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
